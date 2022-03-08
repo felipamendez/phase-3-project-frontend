760978aee432504002c17836d72cb2c252d0ff6f
Felipa Mendez <felipalouise@gmail.com>
2022-03-08 16:02:55 -0600

    cleanned Post comment logic

--- src/Post.js
+++ src/Post.js
@@ -5,33 +5,19 @@ function Post ({post, commentsData}) {
 
     const {content, user, id} = post 
     const {username, photo_src} = user
-    // console.log(user)
     
     //ternary for showing comments button, when click, 
     //logic shows comments if post id = comment post id 
 
     //filters out comments based on post id
 
-    //  If comments.post_id == post_id{return comments}else{ return null}
 
-
-    let filteredComments = commentsData.filter(comment => comment.post_id = id) //dont have access to posts?
-
-     let singleComment = filteredComments.map(comment => (<CommentsPage comment={comment} key={comment.id}/>))
-        //  if (comment.post_id == id) {
-        //      return comment
-        // } else {
-        //     return null
-        // } 
-        //  <CommentsPage comment={comment} key={comment.id}/>
-    //     )
-    // )
+    let filteredComments = commentsData.filter(comment => comment.post_id == id) 
+    let singleComment = filteredComments.map(comment => <CommentsPage comment={comment} key={comment.id}/>)
+    
 
 
     //{showComments ? <CommentDetails comment={comment}/> : null }
-    
-    
-    // console.log(filteredComments)
 
     return (
         <>
@@ -39,7 +25,6 @@ function Post ({post, commentsData}) {
          <p>{username} </p>
          <p>{content}</p>
          {/*hide show comments*/ }
-         {/* <CommentsPage /> */}
          {singleComment}
         </>
     )
