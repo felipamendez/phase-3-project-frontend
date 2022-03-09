54a2bad82ebf59774547ad752e2347a9afc230df
Vanessa Schneider <vanessajwade@gmail.com>
2022-03-08 19:45:27 -0500

    delete working and heart

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,42 @@
 import CommentsPage from "./CommentsPage"
+import React, { useState } from "react";
+import Heart from "react-animated-heart";
 
 function Post ({post, commentsData}) {
-    
-
     const {content, user, id} = post 
     const {username, photo_src} = user
+    const [isClick, setClick]=useState(false)
+    
+
+
+    function handleDelete(){
+        
+            fetch(`http://localhost:9292/posts/${post.id}`, {
+              method: "DELETE",
+            })
+              .then((r) => r.json())
+              .then(() => console.log("deleted!"));
+          }
+
+    function handlePatch(){
+    
+            fetch(`http://localhost:9292/posts/${post.id}`, {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                isClick: !post.isClick,
+              }),
+            })
+              .then((r) => r.json())
+              .then((updatedPost) => console.log(updatedPost));
+
+            setClick((isClick)=>!isClick)
+
+          }
+
+
     
     //ternary for showing comments button, when click, 
     //logic shows comments if post id = comment post id 
@@ -21,11 +53,18 @@ function Post ({post, commentsData}) {
 
     return (
         <>
+        <div>
          <img src={photo_src} className="profile_feed_img"></img>
          <p>{username} </p>
          <p>{content}</p>
+         </div>
+         <div>
+         <button onClick={handleDelete}>Delete Post</button>
+         <Heart isClick={isClick} onClick={() =>setClick(!isClick)}/>
+         </div>
          {/*hide show comments*/ }
          {singleComment}
+        
         </>
     )
 }
